b1297f6db36d567427768dd15172b899e985b024
oncebasun <basunjhm@sina.com>
2016-03-18 20:01:50 +0800

    再次修改了include.mk

--- 14231014-lab/include.mk
+++ 14231014-lab/include.mk
@@ -4,7 +4,10 @@
 # Written By Zhu Like ( zlike@cse.buaa.edu.cn )
 
 
-CROSS_COMPILE :=  bin/mips_4KC-
-CC			  := $(CROSS_COMPILE)gcc
-CFLAGS		  := -O -G 0 -mno-abicalls -fno-builtin -Wa,-xgot -Wall -fPIC
-LD			  := $(CROSS_COMPILE)ld
+CROSS_COMPILE :=  /OSLAB/compiler/usr/bin/mips_4KC-
+CC			  		:= $(CROSS_COMPILE)gcc
+CFLAGS		  	:= -O -G 0 -mno-abicalls -fno-builtin -Wa,-xgot -Wall -fPIC
+LD			  		:= $(CROSS_COMPILE)ld
+
+
+/OSLAB/compiler/usr/bin
